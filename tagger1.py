b6c2c37287436cd8381e6e15db23487882696bd8
braunsteind <braunstein93@gmail.com>
2018-12-03 22:16:47 +0200

    fix tagger1 add test1.pos

--- tagger1.py
+++ tagger1.py
@@ -1,5 +1,5 @@
-STUDENT={'name': 'Coral Malachi_Daniel Braunstein',
-         'ID': '314882853_312510167'}
+STUDENT = {'name': 'Coral Malachi_Daniel Braunstein',
+           'ID': '314882853_312510167'}
 
 import help_funcs as my_help
 import torch
@@ -19,18 +19,55 @@ import sys
 
 NEW_LINE = "\n"
 TAB = "\t"
-#let's define our parameters:
+# let's define our parameters:
 
 learning_rate = 0.01
-window_size = 5 #2 neighbors + target word
+window_size = 5  # 2 neighbors + target word
 DIM_HIDDEN_LAYER = 100
 tags_number = 10
-DIM_INPUT = 250 #window x vector_size = 5 x 50 =250
-NUM_OF_EPOCHS = 1
+DIM_INPUT = 250  # window x vector_size = 5 x 50 =250
+NUM_OF_EPOCHS = 3
 VECTOR_EMBEDDINGS_DIM = 50
 
+BATCH_NORMALIZATION_SIZE = 1024  # denotes the number of samples contained in each generated batch.
 
-BATCH_NORMALIZATION_SIZE = 1024 #denotes the number of samples contained in each generated batch.
+
+###############################################################
+# Function Name:load_dev_data_set
+# Function input:dev_file
+# Function output:dev data set
+# Function Action: loading the dev data set
+#
+################################################################
+
+def load_dev_data_set(dev_file):
+    windows_array, tags = my_help.get_dev_data(dev_file)
+    # Convert a list into a numpy array and set data-type to float32
+    windows_array = np.asarray(windows_array, np.float32)
+
+    # Convert a list into a numpy array and set data-type to int32
+    tags = np.asarray(tags, np.int32)
+
+    """
+    Creates a Tensor from a numpy.ndarray
+    The returned tensor and ndarray share the same memory. Modifications to the tensor will
+     be reflected in the ndarray and vice versa. The returned tensor is not resizable.
+    """
+    tags = torch.from_numpy(tags)
+    windows_array = torch.from_numpy(windows_array)
+
+    # make sure the words and tag have same size, in order to pass it through TensorDataset
+    tags = tags.type(torch.LongTensor)
+    windows_array = windows_array.type(torch.LongTensor)
+
+    data = TensorDataset(windows_array, tags)
+
+    """
+    shuffle is set to True, we will get a new order of exploration at each pass.
+    Shuffling the order in which examples are fed to the classifier is helpful so
+     that batches between epochs do not look alike. Doing so will eventually make our model more robust.
+    """
+    return DataLoader(batch_size=1, shuffle=True, dataset=data)
 
 
 ###############################################################
@@ -42,8 +79,6 @@ BATCH_NORMALIZATION_SIZE = 1024 #denotes the number of samples contained in each
 
 
 def create_match_ploting(m_dev_loss, m_precents_correct_dev):
-
-
     line1, = plt.plot(m_dev_loss.keys(), m_dev_loss.values(), "red",
                       label='Dev avg loss')
 
@@ -60,7 +95,6 @@ def create_match_ploting(m_dev_loss, m_precents_correct_dev):
     plt.show()
 
 
-
 class Part1Model(object):
 
     ###############################################################
@@ -70,15 +104,13 @@ class Part1Model(object):
     # Function Action:The __init__ method is roughly what represents a constructor in Python    #
     ################################################################
 
-    def __init__(self, kind_of_optim,training_set,nn_model,test_data,dev_dataset):
+    def __init__(self, kind_of_optim, training_set, nn_model, test_data, dev_dataset):
 
         self.kind_of_optim = kind_of_optim
         self.training_set = training_set
         self.nn_model = nn_model
         self.test_data = test_data
         self.dev_dataset = dev_dataset
-
-
 
     ###############################################################
     # Function Name:create_predictions_file
@@ -105,8 +137,6 @@ class Part1Model(object):
                     preds_file.write(m_cure_line.strip(NEW_LINE) + " " + list_of_y_hats[m_count] + NEW_LINE)
                     m_count += 1
 
-
-
     ###############################################################
     # Function Name:Get_Representation_Of_Indexes_By_classes
     # Function input:lst_of_index_to_convert
@@ -115,8 +145,6 @@ class Part1Model(object):
 
     def Get_Representation_Of_Indexes_By_classes(self, lst_of_index_to_convert):
         return [my_help.Representation_Of_Indexes_By_classes[i] for i in lst_of_index_to_convert]
-
-
 
     ###############################################################
     # Function Name:train_neural_network
@@ -135,13 +163,13 @@ class Part1Model(object):
         for data, labels in self.training_set:
             self.kind_of_optim.zero_grad()
             output = self.nn_model(data)
-            #call help function to compute the predicted y
+            # call help function to compute the predicted y
             y_hat = get_y_tag(output)
             m_success += y_hat.eq(labels.data.view_as(y_hat)).cpu().sum().item()
-            #computing the loss
+            # computing the loss
             loss = F.nll_loss(output, labels)
             train_loss += loss
-            #start back action
+            # start back action
             loss.backward()
             # updating parameters
             self.kind_of_optim.step()
@@ -151,7 +179,7 @@ class Part1Model(object):
         length = len(self.training_set) * BATCH_NORMALIZATION_SIZE
         accuracy = 100. * m_success / (len(self.training_set) * BATCH_NORMALIZATION_SIZE)
 
-        print_message_each_epoch(1,length,epoch,train_loss,m_success,BATCH_NORMALIZATION_SIZE,accuracy)
+        print_message_each_epoch(1, length, epoch, train_loss, m_success, BATCH_NORMALIZATION_SIZE, accuracy)
 
         ###############################################################
         # Function Name:feedforward_the_dev_set
@@ -162,7 +190,6 @@ class Part1Model(object):
         # #to convert values to be between 0 to 1
 
     ################################################################
-
 
     def feedforward_the_dev_set(self, iter_number, dev_loss_dictionary,
                                 dev_precents_of_accuracy_dictionary, nerOrPos):
@@ -183,20 +210,20 @@ class Part1Model(object):
 
             # check if the mapping type is ner so :
             if nerOrPos == 'ner':
-                if my_help.Representation_Of_Indexes_By_classes[y_hat.cpu().sum().item()] != 'O' or my_help.Representation_Of_Indexes_By_classes[target.cpu().sum().item()] != 'O':
+                if my_help.Representation_Of_Indexes_By_classes[y_hat.cpu().sum().item()] != 'O' or \
+                        my_help.Representation_Of_Indexes_By_classes[target.cpu().sum().item()] != 'O':
                     m_success += y_hat.eq(target.data.view_as(y_hat)).cpu().sum().item()
-                    m_count+=1
+                    m_count += 1
             else:
-                m_count+=1
+                m_count += 1
                 m_success += y_hat.eq(target.data.view_as(y_hat)).cpu().sum().item()
 
         m_loss /= len(self.dev_dataset)
         dev_loss_dictionary[iter_number] = m_loss
-        accuracy =  100. * m_success / m_count
+        accuracy = 100. * m_success / m_count
         dev_precents_of_accuracy_dictionary[iter_number] = accuracy
 
-        print_message_each_epoch(0,m_count,iter_number,m_loss,m_success,1,accuracy)
-
+        print_message_each_epoch(0, m_count, iter_number, m_loss, m_success, 1, accuracy)
 
     ###############################################################
     # Function Name:forward_test_data
@@ -223,10 +250,9 @@ class Part1Model(object):
         # define the path to the test file - from where we'll get the data
         path_to_test_file = ner_or_pos + "/test"
 
-        #call prediction function
+        # call prediction function
         self.create_predictions_file(path_to_test_file, "test1." +
                                      ner_or_pos, pred_list)
-
 
     ###############################################################
     # Function Name:Start_Action
@@ -236,7 +262,6 @@ class Part1Model(object):
     # network and The function produces two graphs that describe the loss of the function and
     # its accuracy percentage for the training set and test set at each iteration
     ################################################################
-
 
     def Start_Action(self, ner_or_pos):
         m_precent_of_accuracy_on_dev = {}
@@ -262,6 +287,7 @@ class NeuralNet(nn.Module):
        embedding vector contains 50 neurons and each window has 5 word
        should be followed by tanh activation function
        """
+
     def __init__(self, input_size):
         super(NeuralNet, self).__init__()
 
@@ -281,7 +307,6 @@ class NeuralNet(nn.Module):
 
     ################################################################
     def forward(self, x):
-
         x = self.E(x).view(-1, self.input_size)
         # use the tanh as the activation function
         x = F.tanh(self.fc0(x))
@@ -289,9 +314,6 @@ class NeuralNet(nn.Module):
         # call softmax function
         x_softmax = F.log_softmax(x, dim=1)
         return x_softmax
-
-
-
 
 
 ###############################################################
@@ -304,7 +326,6 @@ class NeuralNet(nn.Module):
 
 
 def load_training_data_set(train_set_file):
-
     windows_array, tags = my_help.get_train_data(train_set_file)
     # Convert a list into a numpy array and set data-type to float32
     windows_array = np.asarray(windows_array, np.float32)
@@ -334,10 +355,6 @@ def load_training_data_set(train_set_file):
     return DataLoader(batch_size=BATCH_NORMALIZATION_SIZE, shuffle=True, dataset=data)
 
 
-
-
-
-
 ###############################################################
 # Function Name:load_test_set
 # Function input:the file to load contains the test data
@@ -346,10 +363,8 @@ def load_training_data_set(train_set_file):
 #
 ################################################################
 def load_test_set(file_name):
-
     test_dataset = my_help.bring_test_data(file_name)
     return test_dataset
-
 
 
 ###############################################################
@@ -357,11 +372,10 @@ def load_test_set(file_name):
 # Function input:argv
 # Function output:none
 # Function Action:the function load all the data sets wee need for the assignment
-#create a moel of our neural network and start tarining action
+# create a moel of our neural network and start tarining action
 ################################################################
 
 def main(argv):
-
     # ner or pos (user input)
     folder_name_input = argv[0]
     global learning_rate
@@ -369,7 +383,7 @@ def main(argv):
     if folder_name_input == 'ner':
         learning_rate = 0.05
 
-    #define a path for each dataset file
+    # define a path for each dataset file
     path_test = folder_name_input + '/test'
     path_train = folder_name_input + '/train'
     path_dev = folder_name_input + '/dev'
@@ -386,83 +400,48 @@ def main(argv):
     optimizer = optim.Adam(my_neural_network_model.parameters(), lr=learning_rate)
 
     # create a training object
-    training_object = Part1Model(optimizer,set_of_training,my_neural_network_model,dataset_test,set_of_dev)
+    training_object = Part1Model(optimizer, set_of_training, my_neural_network_model, dataset_test, set_of_dev)
 
     # call start action object to start training the model
     training_object.Start_Action(folder_name_input)
 
 
-
 ###############################################################
-#Function Name: get_y_tag
-#Function input: model
-#Function output:return the model prediction tag
-#Function Action: the function return the prediction
-#by getting  the index of the max log-probability
+# Function Name: get_y_tag
+# Function input: model
+# Function output:return the model prediction tag
+# Function Action: the function return the prediction
+# by getting  the index of the max log-probability
 ################################################################
 
 
 def get_y_tag(model):
     return model.data.max(1, keepdim=True)[1]
 
+
 ###############################################################
-#Function Name:print_message_each_epoch
-#Function input:kind_of_set,length of set, loss of model, number
-#of correct predictions of model and size of batch
-#Function output:none
-#Function Action:the function print a message to help the user
-#follow the network progress
-#Function Action:the function calculate the loss of the model
-#for each epochs,and print write message
+# Function Name:print_message_each_epoch
+# Function input:kind_of_set,length of set, loss of model, number
+# of correct predictions of model and size of batch
+# Function output:none
+# Function Action:the function print a message to help the user
+# follow the network progress
+# Function Action:the function calculate the loss of the model
+# for each epochs,and print write message
 ################################################################
 
-def print_message_each_epoch(is_training_set,m_total,epoch,m_loss,m_success,size_of_batch,accuracy):
-    if(is_training_set == 1):
+def print_message_each_epoch(is_training_set, m_total, epoch, m_loss, m_success, size_of_batch, accuracy):
+    if (is_training_set == 1):
         title = "Training Set"
     else:
-        title =  "Validation set"
+        title = "Validation set"
 
-    print('\n'+ title +': Epoch number is:{}  The average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(epoch, m_loss, m_success, m_total,accuracy))
+    print('\n' + title + ': Epoch number is:{}  The average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(epoch,
+                                                                                                              m_loss,
+                                                                                                              m_success,
+                                                                                                              m_total,
+                                                                                                              accuracy))
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
-
-
-###############################################################
-# Function Name:load_dev_data_set
-# Function input:dev_file
-# Function output:dev data set
-# Function Action: loading the dev data set
-#
-################################################################
-
-def load_dev_data_set(dev_file):
-
-    windows_array, tags = my_help.get_dev_data(dev_file)
-    # Convert a list into a numpy array and set data-type to float32
-    windows_array = np.asarray(windows_array, np.float32)
-
-    # Convert a list into a numpy array and set data-type to int32
-    tags = np.asarray(tags, np.int32)
-
-    """
-    Creates a Tensor from a numpy.ndarray
-    The returned tensor and ndarray share the same memory. Modifications to the tensor will
-     be reflected in the ndarray and vice versa. The returned tensor is not resizable.
-    """
-    tags = torch.from_numpy(tags)
-    windows_array = torch.from_numpy(windows_array)
-
-    # make sure the words and tag have same size, in order to pass it through TensorDataset
-    tags = tags.type(torch.LongTensor)
-    windows_array = windows_array.type(torch.LongTensor)
-
-    data = TensorDataset(windows_array, tags)
-
-    """
-    shuffle is set to True, we will get a new order of exploration at each pass.
-    Shuffling the order in which examples are fed to the classifier is helpful so
-     that batches between epochs do not look alike. Doing so will eventually make our model more robust.
-    """
-    return DataLoader(batch_size=1, shuffle=True, dataset=data)
